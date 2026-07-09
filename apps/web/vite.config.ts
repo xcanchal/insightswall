36371bb5dde10bded26ef0085b86051fa768f603
Xavier Canchal <xaviercanchal@gmail.com>
2026-07-09 22:59:59 +0200

    Update Umami analytics config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-const umamiWebsiteId = 'db01975d-983d-4029-a070-4dde6a5da7d3';
+const umamiWebsiteId = '2bddffc3-9476-4488-b112-915d25b7292e';
 const analyticsHosts = ['insightswall.com', 'www.insightswall.com'];
 
 export default defineConfig({
@@ -23,8 +23,9 @@ export default defineConfig({
 			if (${JSON.stringify(analyticsHosts)}.includes(window.location.hostname)) {
 				const script = document.createElement('script');
 				script.defer = true;
-				script.src = 'https://cloud.umami.is/script.js';
+				script.src = 'https://analytics.xaviercanchal.com/script.js';
 				script.dataset.websiteId = '${umamiWebsiteId}';
+				script.dataset.domains = '${analyticsHosts.join(',')}';
 				document.body.appendChild(script);
 			}
 		</script>`;
